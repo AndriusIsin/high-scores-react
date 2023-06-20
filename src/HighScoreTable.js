b7d3c0cf4683cbcd81ddd3e039b36ffbdb8750bd
AndriusIsin <isin.andrius@gmail.com>
2023-06-20 19:57:19 +0100

    level 2 done

--- src/HighScoreTable.js
+++ src/HighScoreTable.js
@@ -3,20 +3,30 @@ const HighScoreTable = (props) => {
   return (
     <div className="main-container">
       <h1>High Scores per Country</h1>
-      {props.results.map((country) => {
-        return (
-          <div className="internal-container" key={country.name}>
-            <h2>HIGH SCORES:{country.name}</h2>
-            {country.scores.map((score, i) => {
-              return (
-                <div className="flexbox" key={i}>
-                  <PlayerScore player={score.n} score={score.s} />
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
+      {props.results
+        .sort((country1, country2) => {
+          if (country1.name < country2.name) {
+            return -1;
+          } else if (country1.name > country2.name) {
+            return 1;
+          } else {
+            return 0;
+          }
+        })
+        .map((country) => {
+          return (
+            <div className="internal-container" key={country.name}>
+              <h2>HIGH SCORES:{country.name}</h2>
+              {country.scores.map((score, i) => {
+                return (
+                  <div className="flexbox" key={i}>
+                    <PlayerScore player={score.n} score={score.s} />
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
     </div>
   );
 };
